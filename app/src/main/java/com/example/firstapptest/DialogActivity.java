5c3350a0041695894d31bc2bd5eff48ef9a337ce
Shenzc <1054694244@qq.com>
2019-07-31 10:51:17 +0800

    step1

---
+++ app/src/main/java/com/example/firstapptest/DialogActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialogActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
--- app/src/main/java/com/example/firstapptest/MainActivity.java
+++ app/src/main/java/com/example/firstapptest/MainActivity.java
@@ -2,14 +2,82 @@ package com.example.firstapptest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "Something you just typed";
+        outState.putString("data_key",tempData);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+        if(savedInstanceState != null){
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d(TAG,tempData);
+        }
         setContentView(R.layout.activity_main);
+        Button startNormalActivity = (Button)findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button)findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(view->{
+            Intent intent = new Intent(MainActivity.this,NormalActivity.class);
+            startActivity(intent);
+        });
+        startDialogActivity.setOnClickListener(view->{
+            Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 
 
