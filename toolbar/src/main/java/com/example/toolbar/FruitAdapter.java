63bad67f36e392ecbd56adbfc815331a807f4432
szc <1054694244@qq.com>
2019-08-09 17:48:47 +0800

    UI

--- toolbar/src/main/java/com/example/toolbar/FruitAdapter.java
+++ toolbar/src/main/java/com/example/toolbar/FruitAdapter.java
@@ -1,9 +1,17 @@
 package com.example.toolbar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -16,8 +24,43 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
 
     static class ViewHolder extends RecyclerView.ViewHolder{
 
-        CardView
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
 
+        public ViewHolder(View view){
+            super(view);
+            cardView = (CardView)view;
+            fruitImage = view.findViewById(R.id.fruit_image);
+            fruitName = view.findViewById(R.id.fruit_name);
+        }
     }
 
+
+    public FruitAdapter(List<Fruit> fruitList){
+        fruits = fruitList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(context == null){
+            context = parent.getContext();
+        }
+        View view = LayoutInflater.from(context).inflate(R.layout.fruit_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = fruits.get(position);
+        holder.fruitName.setText(fruit.getName());
+        Glide.with(context).load(fruit.getImageId()).into(holder.fruitImage);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return fruits.size();
+    }
 }
